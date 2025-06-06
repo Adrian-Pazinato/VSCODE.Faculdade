42261a291ff40c21305172dbcad5f290b138e770
Adrian Pazinato <adriandavida@outlook.com>
2025-06-06 10:51:45 -0300

    printf("E subtraindo 1325 fica: %d\n", x - 1325);

--- defineOperadores.c
+++ defineOperadores.c
@@ -3,11 +3,15 @@
 #define numero 1323
 
 int main() {
+    int x;
+    x = numero;
+
     printf("Hello, World de cabaços!\n");
     printf("O numero e: %d\n", numero);
-    printf("E somando com 15123 fica: %d\n", numero + 15123);
+    printf("E somando com 15123 fica: %d\n", x + 15123);
+    printf("E subtraindo 1325 fica: %d\n", x - 1325);
     printf("E multiplicando por 74 fica: %d\n", numero * 74);
-    printf("E dividindo por 3 fica: %d\n", numero / 3);
+    printf("E dividindo por 3 fica: %d\n", x / 3);
     getchar();
     return 0;
 }
