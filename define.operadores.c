1337e1d48ec2c08d5c7158aa9c2e8af7b37ef25a
Adrian Pazinato <adriandavida@outlook.com>
2025-06-06 10:44:30 -0300

    Hello, World de cabaços!

---
+++ define.operadores.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+#define numero 1323
+
+int main() {
+    printf("Hello, World de cabaços!\n");
+    printf("O numero e: %d\n", numero);
+    printf("E somando com 15123 fica: %d\n", numero + 15123);
+    printf("E multiplicando por 74 fica: %d\n", numero * 74);
+    printf("E dividindo por 3 fica: %d\n", numero / 3);
+    getchar();
+    return 0;
+}
