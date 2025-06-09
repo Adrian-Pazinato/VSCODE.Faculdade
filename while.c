dbdd79c7089988d793e5631c9e641f5b7d23ac82
Adrian Pazinato <adriandavida@outlook.com>
2025-06-09 07:38:53 -0300

    getchar();

--- while.c
+++ while.c
@@ -2,9 +2,11 @@
 
 int main() {
     int res, x = 1;
-    while (x <= 11) {
+    while (x <= 15) {
         res = 3 * x;
         printf("Res = %d\n", res);
         x++;
     }
+    getchar();
+    return 0;
 }
