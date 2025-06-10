25d4f1d8416d0a47a5e754c47d37a2084c7c6f5f
Adrian Pazinato <adriandavida@outlook.com>
2025-06-10 08:56:00 -0300

    CDI

---
+++ valorCDI.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main () {
+    int CDI, MercadoPago, ValorConta, ResultMes, ResultAno;
+    printf("Digite o valor do CDI: ");
+    scanf("%d", &CDI);
+    printf("Digite o valor da conta: ");
+    scanf("%d", &ValorConta);
+
+    
+}
